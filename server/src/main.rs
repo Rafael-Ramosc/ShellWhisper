d337b05398c69bbebab4a4fc374bea4031d72851
Rafael-Ramosc <rafael.ramosrc@gmail.com>
2024-12-30 10:23:38 -0300

    feat: receive connections and create a list with connections

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use tokio::sync::Mutex; 
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let server_adress = "127.0.0.1:8080".to_string();
+    println!("Server adress: {}", &server_adress);
+    let listener = TcpListener::bind(server_adress).await?;
+
+    let connections = Arc::new(Mutex::new(HashMap::new())); 
+    let mut id_counter = 0;
+
+    // accept connections
+    loop {
+        let (socket, addr) = listener.accept().await?;
+        println!("{} is connecting...", addr);
+
+        let connections = connections.clone();
+        let id = id_counter;
+        id_counter += 1;
+
+        // new task
+        tokio::spawn(async move {
+            handle_connection(socket, connections, id).await;
+        });
+    }
+}
+
+async fn handle_connection(
+    socket: tokio::net::TcpStream, 
+    connections: Arc<Mutex<HashMap<i32, SocketAddr>>>,
+    id: i32
+) {
+    if let Ok(addr) = socket.peer_addr() {
+        println!("{} connection succeed", &addr);
+        
+        let mut conn_map = connections.lock().await;
+        conn_map.insert(id, addr);
+   
+        println!("Active connections: {:?}", *conn_map);
+    }
 }
