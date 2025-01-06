3240f7689997d2a90ae69852a23a6f062b7288a9
Rafael-Ramosc <rafael.ramosrc@gmail.com>
2025-01-06 08:59:18 -0300

    feat: add dotenv support for environment variables and update server state management

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,16 +6,33 @@ use tokio::net::TcpListener;
 use std::sync::Arc;
 use server_state::State;
 use handles::handle_connection;
+use dotenv::dotenv;
+use std::env;
 
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    dotenv().ok();
+    
     let server_adress = "127.0.0.1:8080".to_string();
     println!("Server adress: {}", &server_adress);
     let listener = TcpListener::bind(server_adress).await?; 
 
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+
     let server_limit_connections: u32 = 30;
-    let state = Arc::new(State::new(server_limit_connections));
+    let state = Arc::new(
+        State::new(server_limit_connections, &database_url)
+            .await
+            .expect("Failed to create state with database connection")
+    );
+
+    state.test_connection().await
+    .expect("Failed to connect to database");
+
+    println!("Successfully connected to database");
 
     // accept connections
     loop {
--- server/src/server_state.rs
+++ server/src/server_state.rs
@@ -1,20 +1,27 @@
 use tokio::sync::Mutex;
 use std::{collections::HashMap, net::SocketAddr, sync::Arc};
+use sqlx::PgPool;
 
 
 pub struct State {
     pub connection_list: Arc<Mutex<HashMap<u32, SocketAddr>>>,
     pub id_counter: Arc<Mutex<u32>>,
-    max_connections: u32, 
+    max_connections: u32,
+    pub db_pool: PgPool,
 }
 
 impl State {
-    pub fn new(server_limit_connection: u32) -> Self {
-        State {
+
+    pub async fn new(server_limit_connection: u32, database_url: &str) ->  Result<Self, sqlx::Error> {
+
+        let pool = PgPool::connect(database_url).await?;
+
+        Ok(State {
             connection_list: Arc::new(Mutex::new(HashMap::new())),
             id_counter: Arc::new(Mutex::new(0)),
             max_connections: server_limit_connection,
-        }
+            db_pool: pool,
+        })
     }
 
     pub async fn can_accept_connection(&self) -> bool {
@@ -28,4 +35,10 @@ impl State {
         *counter += 1;
         id
     }
+
+    pub async fn test_connection(&self) -> Result<(), sqlx::Error> {
+        sqlx::query("SELECT 1").execute(&self.db_pool).await?;
+        Ok(())
+    }
+
 }
